d90a1f1b3e9c671ad642c42b94a7c9ebf8511533
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-12-11 15:14:33 +0100

    add template for post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,7 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import sanityClient from "../client.js";
+import { Link } from "react-router-dom";
 
 const Post = () => {
-  return <div>Post</div>;
+  const [postData, setPostData] = useState(null);
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[_type == "post"] {
+    title, 
+    slug, 
+    mainImage{
+      asset-> {
+        _id,
+         url
+      },
+      alt
+    }
+  }`
+      )
+      .then((data) => setPostData(data))
+      .catch(console.error);
+  });
+
+  return (
+    <main>
+      <section>
+        <h1></h1>
+        <h3></h3>
+        <div>
+          <article>
+            <Link>
+              <span>
+                <img />
+                <span>
+                  <h3></h3>
+                </span>
+              </span>
+            </Link>
+          </article>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default Post;
