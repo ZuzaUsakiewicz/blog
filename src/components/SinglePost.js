8ba418ce4c9819f6533e89cf070ab4c218b69c57
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2020-12-15 13:09:43 +0100

    add new dependencies for single post page

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client";
+import imageUrlBuilder from "@sanity/image-url";
+import BlockContent from "@sanity/block-content-to-react";
+
+const builder = imageUrlBuilder(sanityClient);
+function urlFor(source) {
+  return builder.image(source);
+}
 
 const SinglePost = () => {
-  return <div>SinglePost</div>;
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
+
+  useEffect(() => {
+    sanityClient
+      .fetch(
+        `*[slug.current== "${slug}"] {
+    title,
+    _id,
+    slug,
+    mainImage{
+      asset->{
+        _id,
+        url
+      }
+    },
+    body,
+    "name": author->name,
+    "authorImage": author->image
+  }`
+      )
+      .then((data) => setSinglePost(data[0]))
+      .catch(console.error);
+  }, [slug]);
+
+  if (!singlePost) return <div>Loading...</div>;
+
+  return (
+    <main>
+      <article>
+        <header>
+          <div>
+            <div>
+              <h1>{singlePost.title}</h1>
+              <div>
+                <img
+                  src={urlFor(singlePost.authorImage).width(200).url()}
+                  alt={singlePost.name}
+                />
+                <p>{singlePost.name}</p>
+              </div>
+            </div>
+          </div>
+          <img src={singlePost.mainImage.asset.url} alt={singlePost.title} />
+        </header>
+        <div>
+          <BlockContent
+            blocks={singlePost.body}
+            projectId="8wj83y2h"
+            dataset="production"
+          />
+        </div>
+      </article>
+    </main>
+  );
 };
 
 export default SinglePost;
